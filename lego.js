fc08bb8f37f17e5adb7c9a5f0e4849a6e4d83312
eoL95 <bragin.egor.95@gmail.com>
2016-11-04 19:37:14 +0500

    Переписал некоторые функции более эффективно

--- lego.js
+++ lego.js
@@ -1,39 +1,29 @@
 'use strict';
 
-var FUNCTION_PRIORITY = ['and', 'or', 'filterIn', 'sortBy', 'select', 'limit', 'format'];
+var FUNCTION_PRIORITY = ['or', 'and', 'filterIn', 'sortBy', 'select', 'limit', 'format'];
 exports.isStar = true;
 
 function copyCollection(collection) {
-    var newCollection = [];
-
     if (collection === undefined) {
         return [];
     }
 
-    collection.forEach(function (note) {
-        var newNote = {};
-        for (var key in note) {
-            if (note[key] !== undefined) {
-                newNote[key] = note[key];
-            }
-        }
-        newCollection.push(newNote);
+    return collection.map(function (note) {
+        return (Object.assign({}, note));
     });
-
-    return newCollection;
 }
 
 exports.query = function (collection) {
     var newCollection = copyCollection(collection);
-    var functions = [].slice.call(arguments).slice(1);
+    var functions = [].slice.call(arguments, 1);
 
     functions.sort(function (a, b) {
         return FUNCTION_PRIORITY.indexOf(a.name) - FUNCTION_PRIORITY.indexOf(b.name);
     });
 
-    for (var i = 0; i < functions.length; i++) {
-        newCollection = functions[i](newCollection);
-    }
+    functions.forEach(function (func) {
+        newCollection = func(newCollection);
+    });
 
     return newCollection;
 };
@@ -42,19 +32,15 @@ exports.select = function () {
     var selectedFields = [].slice.call(arguments);
 
     return function select(collection) {
-        var newCollection = [];
-
-        collection.forEach(function (note, index) {
-            newCollection.push({});
-
+        return collection.map(function (note) {
+            var newNote = {};
             selectedFields.forEach(function (field) {
                 if (note.hasOwnProperty(field) && note[field] !== undefined) {
-                    newCollection[index][field] = note[field];
+                    newNote[field] = note[field];
                 }
             });
+            return newNote;
         });
-
-        return newCollection;
     };
 };
 
@@ -81,22 +67,20 @@ exports.sortBy = function (property, order) {
                 return firstNote[property] > secondNote[property];
             }
 
-            return firstNote[property] < secondNote[property];
+            return secondNote[property] < firstNote[property];
         });
     };
 };
 
 exports.format = function (property, formatter) {
     return function format(collection) {
-        var newCollection = copyCollection(collection);
-
-        newCollection.forEach(function (note) {
+        return collection.map(function (note) {
             if (note.hasOwnProperty(property)) {
                 note[property] = formatter(note[property]);
             }
-        });
 
-        return newCollection;
+            return note;
+        });
     };
 };
 
@@ -115,14 +99,14 @@ if (exports.isStar) {
 
         return function or(collection) {
             var newCollection = [];
+            var filteredCollections = filters.map(function (filter) {
+                return filter(collection);
+            });
+
             collection.forEach(function (note) {
-                var isPush = false;
-                filters.forEach(function (filter) {
-                    if (filter(collection).indexOf(note) !== -1) {
-                        isPush = true;
-                    }
-                });
-                if (isPush) {
+                if (filteredCollections.some(function (filteredCollection) {
+                    return filteredCollection.indexOf(note) !== -1;
+                })) {
                     newCollection.push(note);
                 }
             });
