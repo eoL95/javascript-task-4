2328f69b2a31724113a4aef33d60ce123fb35ac2
eoL95 <bragin.egor.95@gmail.com>
2016-11-04 19:40:01 +0500

    lint fix

--- lego.js
+++ lego.js
@@ -39,6 +39,7 @@ exports.select = function () {
                     newNote[field] = note[field];
                 }
             });
+            
             return newNote;
         });
     };
